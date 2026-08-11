7912935860b683bac0693311007e66aeb99cee0b
Aishwarya Shrivastava <shrivastavaishwarya34@gmail.com>
2026-08-11 17:06:23 +0530

    Update jobs.js

--- server/routes/jobs.js
+++ server/routes/jobs.js
@@ -31,7 +31,11 @@ const jobs = router.post('/', authenticationToken, async (req, res) => {
 const fetchJob = router.get('/', authenticationToken, async (req, res) => {
     try{
         const job = await Job.find({ user: req.user.id })
+        if(!job){
+            return res.status(404).json('Job not found')
+        }
         res.status(200).json(job)
+
     } catch(err){
         res.status(500).json({error: err.message})
     }
@@ -41,11 +45,46 @@ const updateJob = router.put('/:id', authenticationToken, async (req, res) => {
     try{
         const job = await Job.findById(req.params.id)
 
-        if(job.user.toString() == req.user.id)
-
         if(!job){
             return res.status(404).json('error: Job not found')
         }
+
+        if(job.user.toString() == req.user.id){
+            const update = await Job.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
+
+            res.status(200).json({
+                message: "Job updated",
+                update})
+        }
+
+        if(!job.user.toString() == req.user.id){
+            res.status(403).json('Not authorized to update this job')
+        }
+        
+
+    } catch(err){
+        res.status(500).json({error: err.message})
+    }
+})
+
+const deleteJob = router.delete('/:id', authenticationToken, async (req, res) => {
+    try{
+    const job = await Job.findById(req.params.id)
+
+    if(!job){
+        return res.status(404).json('Job not found')
+    }
+
+    if(job.user.toString() == req.user.id){
+    
+        const remove = await Job.findByIdAndDelete(req.params.id)
+
+        res.status(200).json({message: 'Job removed successfully'})
+    }
+
+    if(job.user.toString() !== req.user.id){
+            res.status(403).json('Not authorized')
+    }
 
     } catch(err){
         res.status(500).json({error: err.message})
