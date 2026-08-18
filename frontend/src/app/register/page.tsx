4b4346c001eb2707b149ec51444e81bda5adbf69
Aishwarya Shrivastava <shrivastavaishwarya34@gmail.com>
2026-08-18 19:59:43 +0530

    commit

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -1,5 +1,21 @@
 'use client'
 
+import Image from "next/image"
+
+import registerImg from "@/Register-photo.png"
+
+import { ArrowUpIcon } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+
+
 import Link from "next/link"
 
 import { useState } from "react"
@@ -35,190 +51,74 @@ export default function RegisterPage(){
     return (
         <form 
             onSubmit={handleSubmit}>
-
-            <section 
-                
-            >
-        <div className="text-2xl">
-            <h1>Get Started</h1>
-        </div>
-        <div className="w-80 mx-45 mt-55">
-            <div className=" flex flex-col items-center justify-center gap-5">
-            <div className="relative">
-                <input
-                    id="username"
-                    type="text"
-                    value={username}
-                    onChange={(event) => setUsername(event.target.value)}
-                    placeholder=""
-                    className="
-                    peer
-                    w-80
-                    h-11
-                    rounded-lg
-                    border
-                    border-gray-400
-                    px-3
-                    outline-none
-                    "
-                />
-
-                <label
-                    htmlFor="username"
-                    className="
-                    absolute
-                    left-2
-                    top-1/2
-                    -translate-y-1/2
-                    bg-white
-                    px-1
-                    text-gray-500
-                    transition-all
-                    duration-200
-                    pointer-events-none
-
-                    peer-placeholder-shown:top-1/2
-                    peer-placeholder-shown:-translate-y-1/2
-                    peer-placeholder-shown:text-base
-
-                    peer-focus:-top-2
-                    peer-focus:translate-y-0
-                    peer-focus:text-xs
-                    peer-focus:text-green-700
-
-                    peer-not-placeholder-shown:-top-2
-                    peer-not-placeholder-shown:translate-y-0
-                    peer-not-placeholder-shown:text-xs
-                    "
-                >
-                    Username
-                </label>
-            </div>
-
-            <div className="relative">
-                <input
-                    id="email"
-                    type="text"
-                    value={email}
-                    onChange={(event) => setEmail(event.target.value)}
-                    placeholder=""
-                    className="
-                    peer
-                    w-80
-                    h-11
-                    rounded-lg
-                    // border
-                    border-gray-400
-                    px-3
-                    outline-none
+           {/* main container */}
+            <div className="flex h-screen overflow-hidden w-full" >      
+                {/* left container */}
+                <div className="flex h-screen flex-col items-baseline justify-center w-1/2 ml-43.75 gap-7.5">
+                    <h1 className="mb-10 text-[24px]">Get Started Now</h1>
+                    {/* Username */}
+                    <div className="w-101"
                     
-                    "
-                />
-
-                <label
-                    htmlFor="email"
-                    className="
-                    absolute
-                    left-2
-                    top-1/2
-                    -translate-y-1/2
-                    bg-white
-                    px-1
-                    text-gray-500
-                    transition-all
-                    duration-200
-                    pointer-events-none
-
-                    peer-placeholder-shown:top-1/2
-                    peer-placeholder-shown:-translate-y-1/2
-                    peer-placeholder-shown:text-base
-
-                    peer-focus:-top-2
-                    peer-focus:translate-y-0
-                    peer-focus:text-xs
-                    peer-focus:text-green-700
-
-                    peer-not-placeholder-shown:-top-2
-                    peer-not-placeholder-shown:translate-y-0
-                    peer-not-placeholder-shown:text-xs
-                    "
-                >
-                    Email
-                </label>
-            </div>
-
-            <div className="relative">
-                <input
-                    id="password"
-                    type="text"
-                    value={password}
-                    onChange={(event) => setPassword(event.target.value)}
-                    placeholder=""
-                    className="
-                    peer
-                    w-80
-                    h-11
-                    rounded-lg
-                    // border
-                    border-gray-400
-                    px-3
-                    outline-none
+                    >
+                    <p className="ml-2 text-[14px]">Username</p>
+                    <Input
+                        className="w-full"
+                        type="text"
+                        placeholder="Enter Username"
+                        onChange={(event) => {
+                            setUsername(event.target.value)
+                        }}
+                    />
+                    </div>
+                        {/* email */}
+                    <div className="w-101"
                     
-                    "
-                />
+                    >
+                    <p className="ml-2 text-[14px]">Email</p>
+                    <Input
+                        className="w-full"
+                        type="text"
+                        placeholder="Enter Email"
+                        onChange={(event) => {
+                            setEmail(event.target.value)
+                        }}
+                    />
+                    </div>
 
-                <label
-                    htmlFor="password"
-                    className="
-                    absolute
-                    left-2
-                    top-1/2
-                    -translate-y-1/2
-                    bg-white
-                    px-1
-                    text-gray-500
-                    transition-all
-                    duration-200
-                    pointer-events-none
+                    <div className="w-101"
+                    
+                    >
+                    <p className="ml-2 text-[14px]">Password</p>
+                    <Input
+                        className="w-full"
+                        type="password"
+                        placeholder="Enter Password"
+                        onChange={(event) => {
+                            setPassword(event.target.value)
+                        }}
+                    />
+                    </div>
 
-                    peer-placeholder-shown:top-1/2
-                    peer-placeholder-shown:-translate-y-1/2
-                    peer-placeholder-shown:text-base
+                    <div className="flex flex-wrap items-center gap-2 md:flex-row">
+                        <Button type="submit" variant="outline" className="w-102 bg-green-800 transition-colors duration-300 ease-in-out hover:bg-green-900 hover:text-white text-amber-50 h-9">Signup</Button>
+                    </div>
 
-                    peer-focus:-top-2
-                    peer-focus:translate-y-0
-                    peer-focus:text-xs
-                    peer-focus:text-green-700
-
-                    peer-not-placeholder-shown:-top-2
-                    peer-not-placeholder-shown:translate-y-0
-                    peer-not-placeholder-shown:text-xs
-                    "
-                >
-                    Password
-                </label>
-            </div>
-
-            <div className="flex">
-                <input
-                    type="checkbox"
-
-                />
-                <p>I agree to the <Link href="/terms" className="text-blue-600 text-blue-800 transition-colors">terms & policy</Link></p>
-            </div>
-                <div
-                    className=""
-                >
-                <button 
-                    type="submit"
-                    className="bg-green-700 hover:bg-green-800 text-white font-medium py-2 px-4 rounded-2xl transition duration-200 ease-in-out shadow-sm w-80 "
-                >Register</button>
+                    <div className="flex justify-center items-center align-middle">
+                        <p>Have an account? <Link href="/login" className="text-blue-600">Sign in</Link></p>
+                    </div>
                 </div>
-            </div>
-            </div>
-            </section>
-
-        </form>
+                    {/* right container */}
+                <div className="h-screen relative w-1/2">
+                        <Image
+                            src={registerImg}
+                            className="object-cover"
+                            alt="scenery"
+                            fill
+                        />
+                </div>
+           </div>
+           </form>
+           
     )
     
 }
